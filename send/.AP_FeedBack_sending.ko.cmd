8b83f00664676189bd4edbc2c5ed1398b49fee2f
Huzaifa <huzaifaim@gmail.com>
2016-08-24 11:21:41 +0500

    Driver files changed, now checking the flags

--- send/.AP_FeedBack_sending.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/mubeen/Desktop/send/AP_FeedBack_sending.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/mubeen/Desktop/send/AP_FeedBack_sending.ko /home/mubeen/Desktop/send/AP_FeedBack_sending.o /home/mubeen/Desktop/send/AP_FeedBack_sending.mod.o
